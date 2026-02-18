a2fba6ba2ba974c80105611b9792d9ae1f1606d4
Pranavaditya Sheshendra <pranavaditya2603@gmail.com>
2026-02-18 16:53:53 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import ProductList from "./ProductList";
 import "./App.css";
 
 function App() {
+  const [start, setStart] = useState(false);
+
+  if (start) {
+    return <ProductList />;
+  }
+
   return (
-    <div>
+    <div style={{ textAlign: "center", paddingTop: "150px" }}>
       <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+      <p>Welcome to your one stop plant shop</p>
+
+      <button
+        onClick={() => setStart(true)}
+        style={{
+          padding: "12px 20px",
+          fontSize: "18px",
+          cursor: "pointer"
+        }}
+      >
+        Get Started
+      </button>
     </div>
   );
 }
